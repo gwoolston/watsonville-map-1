a4e8e92b223ac0af0e4df285f3d0c266e36e6d3e
gwoolston <gwoolston@me.com>
2024-03-05 14:17:09 -0500

    Update storymap.js
    remove data in attribution

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -479,22 +479,22 @@ $(window).on('load', function() {
    */
   function changeAttribution() {
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
-    var credit = 'View <a href="'
-      // Show Google Sheet URL if the variable exists and is not empty, otherwise link to Chapters.csv
-      + (typeof googleDocURL !== 'undefined' && googleDocURL ? googleDocURL : './csv/Chapters.csv')
-      + '" target="_blank">data</a>';
+    // var credit = 'View <a href="'
+    //   // Show Google Sheet URL if the variable exists and is not empty, otherwise link to Chapters.csv
+    //   + (typeof googleDocURL !== 'undefined' && googleDocURL ? googleDocURL : './csv/Chapters.csv')
+    //   + '" target="_blank">data</a>';
     
-    var name = getSetting('_authorName');
-    var url = getSetting('_authorURL');
+    // var name = getSetting('_authorName');
+    // var url = getSetting('_authorURL');
 
-    if (name && url) {
-      if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
-      credit += ' by <a href="' + url + '">' + name + '</a> | ';
-    } else if (name) {
-      credit += ' by ' + name + ' | ';
-    } else {
-      credit += ' | ';
-    }
+    // if (name && url) {
+    //   if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
+    //   credit += ' by <a href="' + url + '">' + name + '</a> | ';
+    // } else if (name) {
+    //   credit += ' by ' + name + ' | ';
+    // } else {
+    //   credit += ' | ';
+    // }
 
     credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
     if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
