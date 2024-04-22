ddd0f714cc7b7ee682842d01d330a1fad884d1f5
ihdoyle <159194863+ihdoyle@users.noreply.github.com>
2024-04-22 12:38:20 -0700

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -179,7 +179,7 @@ L.control.layers(basemaps).addTo(map);
 
           if (i == k) {
             /* Adds marker-active class, which is orange, to marker k */
-            markers[k]._icon.className += ' marker-active';
+            markers[k]._icon.className += 'marker-active';
           }
         }
       }
