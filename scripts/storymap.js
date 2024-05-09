f6316e8d4af7202065efae6b595c3502c812b403
ihdoyle <159194863+ihdoyle@users.noreply.github.com>
2024-05-09 14:35:50 -0700

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -97,10 +97,6 @@ $(window).on('load', function() {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 });
     
-    var basemap = trySetting('_tileProvider', 'Stamen.TonerLite');
-    L.tileLayer.provider(basemap, {
-       maxZoom: 18,
-
        // Pass the api key to most commonly used parameters
        apiKey: trySetting('_tileProviderApiKey', ''),
        apikey: trySetting('_tileProviderApiKey', ''),
