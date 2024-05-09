09a8c185b20446fe7634ad97dc84d4d1625dbf14
ihdoyle <159194863+ihdoyle@users.noreply.github.com>
2024-05-09 14:26:35 -0700

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -95,11 +95,7 @@ $(window).on('load', function() {
   var basemap = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-});= L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 });
-
     
     // var basemap = trySetting('_tileProvider', 'Stamen.TonerLite');
     // L.tileLayer.provider(basemap, {
